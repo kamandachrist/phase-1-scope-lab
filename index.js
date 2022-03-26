4330b61ca1afbcf7485194a57441525da80075cc
Christ Kamanda <kamandachristo@gmail.com>
2022-03-26 16:05:40 -0400

    prework scope lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+var customerName = `bob`
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    return customerName
+
+}
+
+function setBestCustomer(){
+    if (setBestCustomer){
+        bestCustomer = `not bob`
+    }
+    return bestCustomer 
+}
+
+function overwriteBestCustomer(){
+    if (overwriteBestCustomer){
+        bestCustomer = `maybe bob`
+    }
+    return bestCustomer
+}
+const leastFavoriteCustomer = `christ`
+
+function changeLeastFavoriteCustomer(){
+    if (changeLeastFavoriteCustomer){
+        leastFavoriteCustomer = `kamanda`
+    }
+    return leastFavoriteCustomer
+}
